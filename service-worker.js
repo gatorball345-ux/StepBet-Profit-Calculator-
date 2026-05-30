010f940526478dda2d50e8c2f8203281d29f71b8
gatorball345-ux <gatorball345@gmail.com>
2026-05-29 22:22:03 -0400

    Update service-worker.js
    Full app experience

--- service-worker.js
+++ service-worker.js
@@ -1,34 +1,24 @@
-const CACHE_NAME = 'stepbet-pwa-v1';
-const ASSETS = [
-  './',
-  './index.html',
-  './script.js',
-  './style.css',
-  './manifest.json'
+const CACHE_NAME = "stepbet-app-v1";
+
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script.js"
 ];
 
-self.addEventListener('install', event => {
+self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
   );
 });
 
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys
-          .filter(key => key !== CACHE_NAME)
-          .map(key => caches.delete(key))
-      )
-    )
-  );
-});
-
-self.addEventListener('fetch', event => {
+self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(cached => {
-      return cached || fetch(event.request);
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
     })
   );
 });
