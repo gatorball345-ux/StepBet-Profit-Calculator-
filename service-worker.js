c9eb6171051250078cde2ebe206b04001b0de0b7
gatorball345-ux <gatorball345@gmail.com>
2026-06-18 11:16:48 -0400

    Update service-worker.js
    Corrected link directory

--- service-worker.js
+++ service-worker.js
@@ -1,40 +1,105 @@
-const CACHE_NAME = "stepcat-v145-netlify-20260618-1";
-const CACHE_PREFIXES = ["stepcat-", "stepbet-"];
+const CACHE_NAME = "stepcat-v145-netlify-20260618-3";
 
 const CORE_ASSETS = [
-  "./",
-  "./index.html",
-  "./manifest.json",
-  "./icon-192.png",
-  "./icon-512.png"
+"./",
+"./index.html",
+"./manifest.json",
+"./icon-192.png",
+"./icon-512.png"
 ];
 
 self.addEventListener("install", event => {
-  event.waitUntil((async () => {
-    const cache = await caches.open(CACHE_NAME);
-    await Promise.allSettled(
-      CORE_ASSETS.map(url => cache.add(new Request(url, { cache: "reload" })))
-    );
-    await self.skipWaiting();
-  })());
+event.waitUntil(
+(async () => {
+const cache = await caches.open(CACHE_NAME);
+
+  await Promise.allSettled(
+    CORE_ASSETS.map(url =>
+      cache.add(new Request(url, { cache: "reload" }))
+    )
+  );
+
+  await self.skipWaiting();
+})()
+
+);
 });
 
 self.addEventListener("activate", event => {
-  event.waitUntil((async () => {
-    const keys = await caches.keys();
-    await Promise.all(
-      keys
-        .filter(key => key !== CACHE_NAME && CACHE_PREFIXES.some(prefix => key.startsWith(prefix)))
-        .map(key => caches.delete(key))
-    );
-    await self.clients.claim();
-  })());
+event.waitUntil(
+(async () => {
+const cacheNames = await caches.keys();
+
+  await Promise.all(
+    cacheNames
+      .filter(name => name !== CACHE_NAME)
+      .map(name => caches.delete(name))
+  );
+
+  await self.clients.claim();
+})()
+
+);
 });
 
 async function networkFirst(request, fallbackUrl) {
-  const cache = await caches.open(CACHE_NAME);
-  try {
-    const response = await fetch(request, { cache: "no-store" });
+const cache = await caches.open(CACHE_NAME);
+
+try {
+const response = await fetch(request, { cache: "no-store" });
+
+if (response && response.ok && response.type === "basic") {
+  await cache.put(request, response.clone());
+}
+
+return response;
+
+} catch (error) {
+const cachedResponse = await cache.match(request);
+
+if (cachedResponse) {
+  return cachedResponse;
+}
+
+if (fallbackUrl) {
+  const fallbackResponse = await cache.match(fallbackUrl);
+
+  if (fallbackResponse) {
+    return fallbackResponse;
+  }
+}
+
+throw error;
+
+}
+}
+
+self.addEventListener("fetch", event => {
+const request = event.request;
+
+if (request.method !== "GET") {
+return;
+}
+
+const url = new URL(request.url);
+
+if (url.origin !== self.location.origin) {
+return;
+}
+
+if (request.mode === "navigate") {
+event.respondWith(networkFirst(request, "./index.html"));
+return;
+}
+
+event.respondWith(networkFirst(request));
+});
+
+self.addEventListener("message", event => {
+if (event.data === "SKIP_WAITING") {
+self.skipWaiting();
+}
+});    const response = await fetch(request, { cache: "no-store" });
     if (response && response.ok && response.type === "basic") {
       cache.put(request, response.clone());
     }
