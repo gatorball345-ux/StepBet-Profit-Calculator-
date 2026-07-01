7680eba26b7fe7bb0ddbab41a240c243a03419d6
gatorball345-ux <gatorball345@gmail.com>
2026-07-01 02:24:36 -0400

    Add files via upload
    Disqualification calculation error fix

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "stepcat-v145-draw-unless-disqualified-20260630-18";
+const CACHE_NAME = "stepcat-v145-draw-unless-disqualified-official-earned-fix-20260701-01";
 
 const CORE_ASSETS = [
   "./",
