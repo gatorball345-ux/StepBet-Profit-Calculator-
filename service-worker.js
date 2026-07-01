ea497da5b0b100727b2ec1c7a9f8b1cdfcc38bee
gatorball345-ux <gatorball345@gmail.com>
2026-07-01 19:57:05 -0400

    Add files via upload
    Autosave enabled

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "stepcat-v145-help-settings-panel-cleanup-20260701";
+const CACHE_NAME = "stepcat-v145-settings-autosave-no-save-button-20260701";
 
 const CORE_ASSETS = [
   "./",
