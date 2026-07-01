a959714479b940b130d624ecdc606d98018e6466
gatorball345-ux <gatorball345@gmail.com>
2026-07-01 11:37:38 -0400

    Add files via upload
    Header row button selection toggle correction

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "stepcat-v145-header-buttons-light-orange-locked-20260701";
+const CACHE_NAME = "stepcat-v145-header-buttons-soft-locked-custom-cells-20260701";
 
 const CORE_ASSETS = [
   "./",
